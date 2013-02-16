2134943ad19b0c603a55874ae922a6052696d028
Alexey Kotlyarov <koterpillar@gmail.com>
2013-02-16 14:20:21 +1100

    Generator: generate armories and hospitals.

--- src/Labyrinth/Generate.hs
+++ src/Labyrinth/Generate.hs
@@ -2,8 +2,36 @@ module Labyrinth.Generate (generateLabyrinth) where
 
 import Labyrinth.Map
 
-import System.Random
+import Peeker
+
+import Control.Monad.Random
+import Control.Monad.State
 
 generateLabyrinth :: (RandomGen g) => Int -> Int -> Int -> g -> (Labyrinth, g)
-generateLabyrinth w h p g = (l, g)
-    where l = emptyLabyrinth w h $ take p $ repeat $ Pos 0 0
+generateLabyrinth w h p = runRand (execStateT generate initial)
+    where initial = emptyLabyrinth w h $ take p $ repeat $ Pos 0 0
+
+type LabGen g a = StateT Labyrinth (Rand g) a
+
+isLand :: Cell -> Bool
+isLand c = isLand' $ getP ctype c
+    where isLand' Land = True
+          isLand' _    = False
+
+landCell :: (RandomGen g) => LabGen g Position
+landCell = do
+    cells <- gets allPosCells
+    let land = filter (isLand . snd) cells
+    if land == []
+        then error "cannot generate anything!"
+        else do
+            i <- getRandomR (0, length land - 1)
+            return $ fst $ land !! i
+
+generate :: (RandomGen g) => LabGen g ()
+generate = do
+    -- Put armories and hospitals in random places
+    forM [Armory, Hospital] $ \ct -> sequence_ $ replicate 2 $ do
+        c <- landCell
+        updS (cell c ~> ctype) ct
+    return ()
--- testsuite/TestLabyrinth/Generate.hs
+++ testsuite/TestLabyrinth/Generate.hs
@@ -13,6 +13,28 @@ import Test.Framework
 generateFromSeed :: Int -> Labyrinth
 generateFromSeed = fst . generateLabyrinth 5 6 3 . mkStdGen
 
+isCellType :: CellTypeResult -> Cell -> Bool
+isCellType ct = (ct ==) . ctResult . getP ctype
+
+countByType :: CellTypeResult -> [Cell] -> Int
+countByType ct = length . filter (isCellType ct)
+
 prop_no_unique_cells :: Int -> Bool
-prop_no_unique_cells = (1 /=) . length . (filter is_land) . allCells . generateFromSeed
-    where is_land = (Land ==) . getP ctype
+prop_no_unique_cells l = and $ map (\ct -> noUnique ct cells) allTypes
+    where cells = allCells $ generateFromSeed l
+          noUnique ct = (1 /=) . (countByType ct)
+          allTypes = [ ArmoryR
+                     , HospitalR
+                     , PitR
+                     , RiverR
+                     , RiverDeltaR
+                     ]
+
+prop_has_required_types :: Int -> Bool
+prop_has_required_types l = and $ map (\ct -> typeExists ct cells) requiredTypes
+    where cells = allCells $ generateFromSeed l
+          typeExists ct = (0 <) . (countByType ct)
+          requiredTypes = [ ArmoryR
+                          , HospitalR
+                          , LandR
+                          ]

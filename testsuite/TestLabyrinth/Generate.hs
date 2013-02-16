cb4f4cd11d552cbd15e0994e0fb0f30b05a923ad
Alexey Kotlyarov <koterpillar@gmail.com>
2013-02-16 15:00:51 +1100

    Use Arbitrary for generator testing.

--- testsuite/TestLabyrinth/Generate.hs
+++ testsuite/TestLabyrinth/Generate.hs
@@ -2,6 +2,8 @@
 
 module TestLabyrinth.Generate (htf_thisModulesTests) where
 
+import Control.Monad
+
 import Peeker
 
 import System.Random
@@ -10,8 +12,8 @@ import Labyrinth
 
 import Test.Framework
 
-generateFromSeed :: Int -> Labyrinth
-generateFromSeed = fst . generateLabyrinth 5 6 3 . mkStdGen
+instance Arbitrary Labyrinth where
+    arbitrary = liftM (fst . generateLabyrinth 5 6 3 . mkStdGen) arbitrary
 
 isCellType :: CellTypeResult -> Cell -> Bool
 isCellType ct = (ct ==) . ctResult . getP ctype
@@ -19,9 +21,9 @@ isCellType ct = (ct ==) . ctResult . getP ctype
 countByType :: CellTypeResult -> [Cell] -> Int
 countByType ct = length . filter (isCellType ct)
 
-prop_no_unique_cells :: Int -> Bool
+prop_no_unique_cells :: Labyrinth -> Bool
 prop_no_unique_cells l = and $ map (\ct -> noUnique ct cells) allTypes
-    where cells = allCells $ generateFromSeed l
+    where cells = allCells l
           noUnique ct = (1 /=) . (countByType ct)
           allTypes = [ ArmoryR
                      , HospitalR
@@ -30,9 +32,9 @@ prop_no_unique_cells l = and $ map (\ct -> noUnique ct cells) allTypes
                      , RiverDeltaR
                      ]
 
-prop_has_required_types :: Int -> Bool
+prop_has_required_types :: Labyrinth -> Bool
 prop_has_required_types l = and $ map (\ct -> typeExists ct cells) requiredTypes
-    where cells = allCells $ generateFromSeed l
+    where cells = allCells l
           typeExists ct = (0 <) . (countByType ct)
           requiredTypes = [ ArmoryR
                           , HospitalR
